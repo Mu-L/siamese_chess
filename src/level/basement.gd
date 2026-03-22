e5b48d220c76055ddb4d497df10600f96d2ec337
FaMuLan <fa_mulan@163.com>
2026-03-23 01:21:34 +0800

    控制开关灯

--- src/level/basement.gd
+++ src/level/basement.gd
@@ -1,5 +1,7 @@
 extends Level
 
+var light_switch:bool = false
+
 func _ready() -> void:
 	super._ready()
 	$player.force_set_camera($camera_3d)
@@ -9,3 +11,8 @@ func _ready() -> void:
 	$chessboard.state.add_piece(cheshire_by, player_king)
 	$chessboard.add_piece_instance(cheshire_instance, cheshire_by)
 	chessboard.button_input_pointer = cheshire_by
+	get_tree().call_group("lights", "set_visible", light_switch)
+
+func change_light() -> void:
+	light_switch = !light_switch
+	get_tree().call_group("lights", "set_visible", light_switch)
