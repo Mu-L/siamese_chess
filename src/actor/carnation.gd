81bf26810ddda466065d3a15d6bb6a8adbab7dc9
FaMuLan <fa_mulan@163.com>
2026-04-15 01:00:47 +0800

    给康乃馨增加弹琴姿势

--- src/actor/carnation.gd
+++ src/actor/carnation.gd
@@ -1,7 +1,7 @@
 extends Actor
 
 func _ready() -> void:
-	$animation_tree.get("parameters/playback").start("idle")
+	$animation_tree.get("parameters/playback").start("sit_and_play")
 
 func set_direction(_rotation:float) -> Actor:
 	global_rotation.y = _rotation
