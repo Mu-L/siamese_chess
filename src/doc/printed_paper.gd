9235423b38fa73617f2915716fb618d1d3ba9917
FaMuLan <fa_mulan@163.com>
2026-04-15 21:49:12 +0800

    可翻页的PrintedPaper文档

--- src/doc/printed_paper.gd
+++ src/doc/printed_paper.gd
@@ -1,33 +1,59 @@
 extends Notable
 
-@export var img_path:String = ""
+class PrintedPage extends RefCounted:
+	var path:String = ""
+	var scene:PackedScene = null
+
+var current_page:int = 0
+var current_page_instance:PrintedPage = null
+var instance:Node = null
+
+var page_list:Array = []
 
 func _ready() -> void:
-	if ResourceLoader.exists(img_path):
-		$sprite_2d.texture = load(img_path)
-	else:
-		var image:Image = Image.load_from_file(img_path)
-		$sprite_2d.texture = ImageTexture.create_from_image(image)
+	pass
 
 func parse(data:Dictionary) -> void:
 	super.parse(data)
-	img_path = data["path"]
-	if ResourceLoader.exists(img_path):
-		$sprite_2d.texture = load(img_path)
-	else:
-		var image:Image = Image.load_from_file(img_path)
-		$sprite_2d.texture = ImageTexture.create_from_image(image)
+	var data_arr:Array = data["printed"]
+	for iter:Dictionary in data_arr:
+		var page:PrintedPage = PrintedPage.new()
+		page.path = iter["path"]
+		page.scene = load(page.path)
+		page_list.push_back(page)
+	current_page = 0
+	current_page_instance = page_list[page_index()]
+	instance = current_page_instance.scene.instantiate()
+	add_child(instance)
+
 
 func dict() -> Dictionary:
 	var data:Dictionary = super.dict()
-	data["path"] = img_path
+	var data_arr:Array = []
+	for iter:PrintedPage in page_list:
+		data_arr.push_back({"path": iter.path})
+	data["printed"] = data_arr
 	return data
 
 func get_rect() -> Rect2:
-	return $sprite_2d.get_rect() * $sprite_2d.transform
+	return instance.get_rect()
+
+func new_page() -> void:
+	super.new_page()
+	var page:PrintedPage = PrintedPage.new()
+	page_list.push_back(page)
+	turn_page(page_count() - 1)
+
+func turn_page(_page:int) -> void:
+	super.turn_page(_page)
+	current_page = _page
+	current_page_instance = page_list[current_page]
+	instance.queue_free()
+	instance = current_page_instance.scene.instantiate()
+	add_child(instance)
+
+func page_count() -> int:
+	return page_list.size()
 
 func page_index() -> int:
-	return 0
-
-func page_size() -> int:
-	return 1
+	return current_page
