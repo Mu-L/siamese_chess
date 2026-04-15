53e71603d3395e27935a2abc38166b0ac7a3bb86
FaMuLan <fa_mulan@163.com>
2026-04-15 16:40:44 +0800

    重新设置对话

--- src/actor/carnation.gd
+++ src/actor/carnation.gd
@@ -1,7 +1,7 @@
 extends Actor
 
 func _ready() -> void:
-	$animation_tree.get("parameters/playback").start("sit_and_play")
+	$animation_tree.get("parameters/playback").start("idle")
 
 func set_direction(_rotation:float) -> Actor:
 	global_rotation.y = _rotation
--- src/level/garden.gd
+++ src/level/garden.gd
@@ -1,1 +1,20 @@
 extends Level
+
+func _ready() -> void:
+	super._ready()
+	Ambient.change_environment_sound(load("res://assets/audio/405135__mjeno__autumn-forest-leaves-falling-close-to-pond-iii-loopable.wav"))
+	var cheshire_by:int = get_meta("by")
+	var cheshire_instance:Actor = load("res://scene/actor/cheshire.tscn").instantiate()
+	cheshire_instance.position = $chessboard.x88_to_vector3(cheshire_by)
+	$chessboard.state.add_piece(cheshire_by, player_king)
+	$chessboard.add_piece_instance(cheshire_instance, cheshire_by)
+	chessboard.button_input_pointer = cheshire_by
+	$player.force_set_camera($camera)
+
+func interact_carnation() -> void:
+	Dialog.push_dialog("CARNATION_TALK_0_0", "", true, true)
+	$player.force_set_camera($camera_carnation_dialog)
+	await Dialog.on_next
+	Dialog.push_dialog("CARNATION_TALK_0_1", "", false, true)
+	await Dialog.on_next
+	$player.force_set_camera($camera)
