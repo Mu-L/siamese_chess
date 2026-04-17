84deadde1a88dcbdc6bb6f8d915ecdd2b83b5013
FaMuLan <fa_mulan@163.com>
2026-04-17 18:17:07 +0800

    带上meta一起记录

--- src/startup.gd
+++ src/startup.gd
@@ -2,7 +2,8 @@ extends Control
 
 func _ready() -> void:
 	Progress.load_file()
-	var current_level:String = Progress.get_value("current_level", "res://scene/level/cafe.tscn")
+	var current_level:String = Progress.get_value("current_level", "res://scene/level/entrance.tscn")
+	var current_level_meta:Dictionary = Progress.get_value("current_level", {})
 	if current_level == "res://scene/startup.tscn":
-		current_level = "res://scene/level/cafe.tscn"
-	Loading.change_scene(current_level, {})
+		current_level = "res://scene/level/entrance.tscn"
+	Loading.change_scene(current_level, current_level_meta)
